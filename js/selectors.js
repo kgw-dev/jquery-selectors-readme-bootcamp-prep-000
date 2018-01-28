ae88125bdfeb6cba770368c3861be43d6fa41d29
kent winter <kgw700@gmail.com>
2018-01-28 22:44:36 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,7 +1,17 @@
 // declare your functions here...
+
 function paragraphSelector(){
   $('p');
 }
+
 function lastimageSelector(){
+  $('img:last-child');
+}
+
+function ninjaBabySelector(){
+  $('img:last-child');
+}
+
+function divSelector(){
   $('img:last-child');
 }
